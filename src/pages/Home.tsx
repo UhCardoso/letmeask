7716400568d31cf7bee2194a99c5772e69cf40e4
uhcardoso <werlendev@gmail.com>
2021-06-24 19:42:52 -0300

    nlw-dia-3

---
+++ src/pages/Home.tsx
@@ -0,0 +1,83 @@
+import {FormEvent, useState} from 'react';
+import {useHistory} from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logo from '../assets/images/logo.svg';
+import googleIconImage from '../assets/images/google-icon.svg';
+
+import { database } from '../services/firebase';
+
+import { Button } from '../components/Button';
+
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
+
+export function Home() {
+    const history = useHistory();
+    const {user, signInWithGoogle} = useAuth();
+
+    const [roomCode, setRoomCode] = useState('');
+
+    async function handleCreateRoom() {
+        if(!user) {
+            await signInWithGoogle()
+        }
+
+        history.push('rooms/new');        
+    }
+
+    async function handleJoinRooom(event:FormEvent) {
+        event.preventDefault();
+
+        if(roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()) {
+            alert('Sala não Existe');
+            return;
+        }
+
+        if(roomRef.val().endedAt) {
+            alert("Esta sala foi encerrada.")
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
+    }
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="ilustração perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as duvidas da sua audiência em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logo} alt="letmeask" />
+                    <button onClick={handleCreateRoom} className="create-room">
+                        <img src={googleIconImage} alt="logo do google"/>
+                        Crie a sua sala com o google
+                    </button>
+                    <div className="separator">Ou entre em uma sala</div>
+                    <form onSubmit={handleJoinRooom}>
+                        <input 
+                            type="text"
+                            placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
+                        />
+                        <Button type="submit">
+                            Entrar na sala
+                        </Button>
+                    
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
